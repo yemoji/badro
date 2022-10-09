f5336e4e45b326ea380a983464c63c9acacc3df3
unknown <amirrezamojarad@yahoo.com>
2022-10-09 22:05:36 +0330

    why badro

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,14 @@
 import styled from '@emotion/styled';
 import { Grid } from '@mui/material';
 import HeaderHome from '@src/components/pages/home/Header';
+import WhyBadro from '@src/components/pages/home/whybadro';
 import type { NextPage } from 'next';
 
 const Home: NextPage = () => {
   return (
     <Container container flexDirection="column">
       <HeaderHome />
+      <WhyBadro />
     </Container>
   );
 };
--- src/components/pages/home/whybadro.tsx
+++ src/components/pages/home/whybadro.tsx
@@ -1,9 +1,31 @@
-import { Grid } from '@mui/material';
+import { Button, Grid, Typography } from "@mui/material"
+import styled from "@emotion/styled"
 
 export default function WhyBadro() {
   return (
-    <Grid p={4} container flexDirection="column">
-      
+    <Grid p={1} py={8} rowGap={6} container alignItems="center" flexDirection="column">
+      <Typography variant="h2" fontSize={32} color="secondary.main">
+        چرا از پادرو استفاده کنیم؟
+      </Typography>
+      <Grid container ml="0">
+
+      <SubjectContainer container mx="auto" pink item xs={12} md={7}>
+        123
+      </SubjectContainer>
+      </Grid>
+      <SubjectContainer container mx="auto" item xs={12} md={7}>
+        123
+      </SubjectContainer>
     </Grid>
   )
 }
+
+type SubjectContainerProps = {
+  pink? : boolean;
+}
+
+const SubjectContainer = styled(Grid)<SubjectContainerProps>`
+  background-color: ${p => p.pink ? "#FFF0E5" : "#E5F6FF"};
+  border-radius: 59px;
+  align-self: ${p => p.pink ? "flex-start" : "flex-end"};
+`
